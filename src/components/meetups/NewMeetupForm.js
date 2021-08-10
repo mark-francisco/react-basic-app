23cfa20839a653df40bddd67c48c0720618b63be
Mark Francisco <mfrancisco581@gmail.com>
2021-08-10 00:55:50 -0500

    add submit handler fn

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,9 +2,15 @@ import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
 function NewMeetupForm() {
+  // React will automatically pass the event object as an argument to whatever function gets called from within the form
+  function submitHandler(event) {
+    // in vanilla JS, the event object has a preventDefault() method that prevents the browser from making an HTTP request and reloading the page
+    event.preventDefault();
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
           <input type="text" required id="title" />
