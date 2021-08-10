7411a2cb9a017ea4e3a8b51e692059fdac082388
Mark Francisco <mfrancisco581@gmail.com>
2021-08-10 16:22:53 -0500

    add refs to NewMeetupForm to read the values from input elements

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,11 +1,24 @@
+import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
 function NewMeetupForm() {
-  // React will automatically pass the event object as an argument to whatever function gets called from within the form
+  // create a "reference" object. refs are good for reading values.
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  // for event listener events, React will automatically pass the event object as an argument to whatever function gets called from within the form
   function submitHandler(event) {
     // in vanilla JS, the event object has a preventDefault() method that prevents the browser from making an HTTP request and reloading the page
     event.preventDefault();
+
+    // in React, useRef objects have a .current property that holds the actual value. in JS, input elements have a .value property.
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
   }
 
   return (
@@ -13,19 +26,20 @@ function NewMeetupForm() {
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          {/* all React elements come built in with the ref prop. The ref prop binds the input element's value with the associated JS variable */}
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Meetup Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Meetup Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea id="description" required rows="5" ref={descriptionInputRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
