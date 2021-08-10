7af4e7d85bee3312c8c3b69249d2399fd64b124d
Mark Francisco <mfrancisco581@gmail.com>
2021-08-09 23:35:51 -0500

    use props.children to apply styles Main and Card throughout the app

--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -3,7 +3,7 @@ import classes from "./MeetupList.module.css";
 
 function MeetupList(props) {
   return (
-    <ul className={classes.List}>
+    <ul className={classes.list}>
       {props.meetups.map((meetup) => (
         <MeetupItem
           key={meetup.id}
--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,7 +1,8 @@
 import classes from "./Card.module.css";
 
-function Card() {
-  return <div className={classes.card}></div>;
+function Card(props) {
+  // every component received the "children" prop by default. "children" holds all of the JSX content that's between the opening and closing tag of this component where it's used.
+  return <div className={classes.card}>{props.children}</div>;
 }
 
 export default Card;
