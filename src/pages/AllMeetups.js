0a5241db78910bd67f32db0f0771a0bc66d950b7
Mark Francisco <mfrancisco581@gmail.com>
2021-08-10 23:16:50 -0500

    add fetch api logic to get all meetups from firebase and add new meetup to firebase. use useEffect hook to load all meetups upon completion of fetch api GET

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,30 +1,50 @@
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
-
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  // the .useEffect() hook lets you define conditions for when certain code should be run. 2 arguments: (fn that should be conditionally run, array of dependencies). Under useEffect, the fn will run as long as the values in the array of dependencies have not changed since the last fn execution.
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch("https://react-basic-app-43c81-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        // .json() converts a JSON response to a plain JS object. since response.json() returns another promise, we must do another .then to access the actual data once response.json() is resolved.
+        return response.json();
+      })
+      .then((data) => {
+        // "data" fetched from firebase is returned as a single object, where each meetup in the collection is a key on that object. each key then contains, as its value, the list of meetup properties. we must transform this data to a JS array.
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            // JS spread operator adds all of the meetup attributes & values inside each "key" into this new meetup object.
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
       {/* React CAN render arrays of JS objects as JSX elements. The .map fn in JS returns an array of transformed objects. */}
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,25 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(meetupData) {}
+  // useHistory() hook gives us a history object. the history object allows us to manipulate the browser's history.
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    // in vanilla JS, the fetch fn allows you to send http requests.
+    // firebase allows you to append onto the URL to specify collections for your data. firebase requires that your API URLs end with ".json"
+    fetch("https://react-basic-app-43c81-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      // JSON.stringify converts JS objects to JSON strings
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      // history.replace() navigates you to a different route, without allowing for the use of .back().
+      history.replace("/");
+    });
+  }
 
   return (
     <section>
