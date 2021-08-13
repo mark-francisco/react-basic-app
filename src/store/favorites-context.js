1a28c02435fa0f50ddf38adf45b946b4cf42a45e
Mark Francisco <mfrancisco581@gmail.com>
2021-08-13 12:55:44 -0500

    start using createContext in order to track multi-component state

---
+++ src/store/favorites-context.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+// createContext() creates a JS object. This is used for managing state that affects multiple components/parts of the application.
+createContext();
